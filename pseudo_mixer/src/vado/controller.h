e5417c6d8cd6f4b9b9679df2d491133e6ad00cbb
Russell Keane <russell.keane@inps.co.uk>
2018-12-13 20:10:41 +0000

    move controller consts out to the constants file

--- pseudo_mixer/src/vado/controller.h
+++ pseudo_mixer/src/vado/controller.h
@@ -22,41 +22,6 @@ class Controller
     bool parseTemperature(const byte, int& temperature);
     bool parseFlow(const byte, int& flow);
     bool parseOutlet(const byte, int& outlet);
-
-    //Controller message details
-    //Message byte positions
-    static const byte BYTE_1 = 0;
-    static const byte BYTE_2 = 1;
-    static const byte BYTE_POWER = 2;
-    static const byte BYTE_TEMP = 3;
-    static const byte BYTE_FLOW = 4;
-    static const byte BYTE_OUTLET = 5;
-    static const byte BYTE_END = 6;
-
-    //Power definitions
-    static const byte POWER_ON = 0x01;
-    static const byte POWER_OFF = 0x02;
-    static const byte POWER_PAUSED = 0x03;
-
-    //Temperature definitions
-    static const int TEMP_OFFSET = 16; //the value sent from the controller is 16 higher than the deg C value
-    //Signal MIN and MAX
-    static const byte TEMP_MIN = 0x29;
-    static const byte TEMP_MAX = 0x3E;
-    //Converted MIN and MAX
-    static const byte TEMP_MIN_C = 25;
-    static const byte TEMP_MAX_C = 46;
-
-    //Flow definition
-    static const byte FLOW_MIN = 0x05;
-    static const byte FLOW_LOW = 0x06;
-    static const byte FLOW_MED = 0x07;
-    static const byte FLOW_HIGH = 0x08;
-    static const byte FLOW_MAX = 0x09;
-
-    //Outlet definitions
-    static const byte OUTLET_DEFAULT = 0x02;
-    static const byte OUTLET_ALTERNATIVE = 0x01;
 };
 
 #endif
